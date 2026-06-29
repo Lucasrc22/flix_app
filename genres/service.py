45d425f3e861d9ac51ec2ea15b48352aa64bd0a6
LucasRC22 <lucasrios2210@gmail.com>
2026-06-29 11:47:09 -0300

    atualizando repository e service

--- genres/service.py
+++ genres/service.py
@@ -1,25 +1,15 @@
-import requests
-import streamlit as st
-from login.service import logout
+from genres.repository import GenreRepository
 
-class GenreRepository:
-
+class GenreService:
     def __init__(self):
-        self.__base_url = 'http://127.0.0.1:8000/api/v1/'
-        self.__genres_url = f'{self.__base_url}genres/'
-        self.__headers = {
-            'Authorization': f'Bearer {st.session_state.token}'
-        }
+        self.genre_repository = GenreRepository()
 
     def get_genres(self):
-        response = requests.get(
-            self.__genres_url,
-            headers=self.__headers,
+        return self.genre_repository.get_genres()
+    
+    def create_genres(self, name):
+        genre = dict(
+            name=name,
         )
-        if response.status_code == 200:
-            return response.json()
-        if response.status_code == 401:
-            logout()
-            return None
-        raise Exception(f'Erro ao obter dados da API. Status Code: {response.status_code}')
-        
+        return self.genre_repository.create_genre(genre)
+    
